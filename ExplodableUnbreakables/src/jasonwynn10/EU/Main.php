c9d00b798acf5404c3ee897ce360027c5bb82959
ZPlayzMCPE <jameshayward3800@gmail.com>
2017-12-09 16:52:08 +0000

    Update Main.php

--- ExplodableUnbreakables/src/jasonwynn10/EU/Main.php
+++ ExplodableUnbreakables/src/jasonwynn10/EU/Main.php
@@ -17,7 +17,7 @@ class Main extends PluginBase implements Listener {
 	/** @var Block[] $blocks */
 	private $blocks = [];
 	public function onEnable() {
-		Main extends Bedrock {
+		class Main extends Bedrock {
 			public function getBlastResistance() : float {
 				return 36.41; // accounts for 2 steps out from explosion
 			}
